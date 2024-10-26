7cd9120e0ea6a5ced829c2f9e224c47102ca9a4e
Yuankai Luo <yuankai0220@163.com>
2024-10-26 22:05:43 +0800

    init

--- large_graph/arxiv.sh
+++ large_graph/arxiv.sh
@@ -1,5 +1,5 @@
 #gcn
-python main-arxiv.py --dataset ogbn-arxiv --hidden_channels 512 --epochs 2000 --lr 0.0005 --runs 2 --local_layers 4 --bn --device 0 --res 
+python main-arxiv.py --dataset ogbn-arxiv --hidden_channels 512 --epochs 2000 --lr 0.0005 --runs 2 --local_layers 5 --bn --device 0 --res 
 
 #sage
 python main-arxiv.py --dataset ogbn-arxiv --hidden_channels 256 --epochs 2000 --lr 0.0005 --runs 2 --local_layers 4 --bn --device 0 --res --sage
